377a65ea7be0fcd07deecebfa825428e7782b41f
Whilmar Bitoco <117832470+whilmarbitoco@users.noreply.github.com>
2023-01-30 10:25:56 +0000

    Sorted the files

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-import json, sys, os
-import dessert, dish, log, login, signup
+import sys, os
+from bin.modules import dessert, dish, log, login, signup
+
 
 def menu():
   print("[1] Dessert")
   print("[2] Main dish")
   print("\n")
-  
+
+
 def console():
   print("[1] Menu")
   print("[2] Transaction")
   print("[3] Exit")
   print("\n")
-  
+
+
 def store():
   global username
   menu()
   c = "[Choose]► "
   user = input(c)
   print("\n")
-  
+
   if user == "1":
     dessert.getall()
     print("\n")
     c = "[Choose]► "
     user = int(input(c))
-    
+
     if dessert.getid(user):
       fund = int(input("[Money]► "))
       price = dessert.getid(user)
-      
+
       if price is not None:
         if fund >= price:
           quan = input("[Quantity]► ")
@@ -39,7 +42,7 @@ def store():
           print("[2] NO")
           cfm = input("[Confirm]► ")
           print("\n")
-          
+
           if cfm == "1":
             total = fund - price
             gin = dessert.getnamebyid(user)
@@ -47,27 +50,27 @@ def store():
             print("[!] Transaction successful")
             print(f"[!] Change: {total}")
             print("\n")
-            
+
           elif cfm == "2":
             print("[!] Order Cancelled")
-            
+
           else:
             print("[!] Choice in choices")
-          
+
         else:
           print("[!] Insufficient funds")
           print("\n")
-   
+
   elif user == "2":
     dish.getall()
     print("\n")
     c = "[Choose]► "
     user = int(input(c))
-    
+
     if dessert.getid(user):
       fund = int(input("[Money]► "))
       price = dish.getid(user)
-      
+
       if price is not None:
         if fund >= price:
           quan = input("[Quantity]► ")
@@ -78,7 +81,7 @@ def store():
           print("[2] No")
           cfm = input("[Confirm]► ")
           print("\n")
-          
+
           if cfm == "1":
             total = fund - price
             gin = dessert.getnamebyid(user)
@@ -86,35 +89,35 @@ def store():
             print("[!] Transaction successful")
             print(f"[!] Change: {total}")
             print("\n")
-            
+
           elif cfm == "2":
             print("[!] Order Cancelled")
-            
+
           else:
             print("[!] Choice in choices")
-          
+
         else:
           print("[!] Insufficient funds")
           print("\n")
-  
+
   else:
     print("[!] Choice in choices")
 
-  
+
 def main():
   console()
   c = input("[Choose]► ")
   if c == "1":
     store()
-    
+
   elif c == "2":
     global username
     log.getlog(username)
     print("\n")
-  
+
   elif c == "3":
     sys.exit()
-    
+
   else:
     print("[!] Choice in choices")
 
@@ -129,20 +132,20 @@ while True:
   c = input("[Choose]► ")
   if c == "1":
     username = input("username: ")
-    password = input ("password: ")
+    password = input("password: ")
     if login.getcred(username, password):
       print(f"[!] Welcome to Uncrowned Kings Market, {username} [!]")
       print("\n")
       while login.getcred(username, password):
         main()
-    
+
     else:
-      print ("[!] User not found")
+      print("[!] User not found")
       print("\n")
-      
+
   elif c == "2":
     username = input("username: ")
-    password = input ("password: ")
+    password = input("password: ")
     signup.signup(username, password)
     print("[!] Account Created")
 
@@ -151,6 +154,3 @@ while True:
 
   else:
     print("[!] hoice in choices")
-    
-  
-  
